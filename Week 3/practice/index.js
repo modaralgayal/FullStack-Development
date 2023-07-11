bd5d262bbd3a8cc9bb70e26fbb91b42e6e9b2413
modar_algayal <mudar.algayal@helsinki.fi>
2023-07-11 17:02:24 +0300

    Update

--- Week 3/practice/index.js
+++ Week 3/practice/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 app.use(express.json())
+app.use(cors())
 
 let notes = [
   {
@@ -72,6 +74,7 @@ app.post('/api/notes', (request, response) => {
 })
 
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
